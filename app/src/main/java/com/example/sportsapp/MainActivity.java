a9c842609122bd737b288338d81cbcd11964ed80
Aodan <hardy-a4@ulster.ac.uk>
2024-11-18 20:11:04 +0000

    activities

--- app/src/main/java/com/example/sportsapp/MainActivity.java
+++ app/src/main/java/com/example/sportsapp/MainActivity.java
@@ -2,13 +2,55 @@ package com.example.sportsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button addLeagueToDb, searchForClubByLeague,searchForClub;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addLeagueToDb = findViewById(R.id.btnAddLeaguesToDb);
+        searchForClubByLeague = findViewById(R.id.btnSearchForClubsByLeague);
+        searchForClub = findViewById(R.id.btnSearchForClubs);
+
+
+        addLeagueToDb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AddLeaguesToDb.class);
+
+                startActivity(intent);
+
+
+            }
+        });
+
+        searchForClubByLeague.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SearchClubByLeague.class);
+
+                startActivity(intent);
+
+            }
+        });
+
+
+        searchForClub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SearchForClubs.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
