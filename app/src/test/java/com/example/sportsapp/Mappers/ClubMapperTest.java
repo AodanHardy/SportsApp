370993b8c79af4f159180e4ac183121811ce5cbb
Aodan <hardy-a4@ulster.ac.uk>
2024-12-03 12:44:46 +0000

    ui improvments

---
+++ app/src/test/java/com/example/sportsapp/Mappers/ClubMapperTest.java
@@ -0,0 +1,17 @@
+package com.example.sportsapp.Mappers;
+
+import com.example.sportsapp.Database.Club;
+
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class ClubMapperTest {
+
+}
